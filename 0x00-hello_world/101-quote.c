cdf846572796a9761a129f5753d47f574060e06b
dennis-tech <irungudennis956@yahoo.com>
2022-06-03 01:07:34 +0000

    added last file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - my entry point
- * Description:'and that piece of art is useful" - Dora korpar, 2015-10-19'
+ * Description: 'and that piece of art is useful" - Dora korpar, 2015-10-19'
  * Return: 1
  */
 int main(void)
